2f48d6692c2a21aaaab3e671c8fa0fc278a7ac9a
mcagabe19 <egzozu.be.bas@gmail.com>
2024-12-15 18:31:38 +0000

    first commit

---
+++ src/vlc/MP4Sprite.hx
@@ -0,0 +1,80 @@
+package;
+
+import flixel.FlxSprite;
+import flixel.graphics.FlxGraphic;
+import flixel.util.FlxColor;
+import vlc.MP4Handler;
+
+/**
+ * This class allows you to play videos using sprites (FlxSprite).
+ */
+class MP4Sprite extends FlxSprite
+{
+	public var bitmap:MP4Handler;
+	public var canvasWidth:Null<Int>;
+	public var canvasHeight:Null<Int>;
+	public var fillScreen:Bool = false;
+
+	public var openingCallback:Void->Void = null;
+	public var finishCallback:Void->Void = null;
+
+	public function new(X:Float = 0, Y:Float = 0)
+	{
+		super(X, Y);
+
+		makeGraphic(1, 1, FlxColor.TRANSPARENT);
+
+		bitmap = new MP4Handler();
+		bitmap.canUseAutoResize = false;
+		bitmap.alpha = 0;
+		bitmap.openingCallback = function()
+		{
+			if (openingCallback != null)
+				openingCallback();
+		}
+		bitmap.finishCallback = function()
+		{
+			oneTime = false;
+			if (finishCallback != null)
+				finishCallback();
+
+			kill();
+		}
+	}
+
+	private var oneTime:Bool = false;
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		if (bitmap.isPlaying && bitmap.isDisplaying && bitmap.bitmapData != null && !oneTime)
+		{
+			var graphic:FlxGraphic = FlxG.bitmap.add(bitmap.bitmapData, false, '');
+			if (graphic.imageFrame.frame == null)
+			{
+				trace('the frame of the image is null?');
+				return;
+			}
+
+			loadGraphic(graphic);
+			if (canvasWidth != null && canvasHeight != null)
+			{
+				setGraphicSize(canvasWidth, canvasHeight);
+				updateHitbox();
+
+				var size:Float = (fillScreen ? Math.max : Math.min)(sprite.scale.x, sprite.scale.y);
+				scale.set(size, size); // lol
+			}
+			oneTime = true;
+		}
+	}
+
+	/**
+	 * Native video support for Flixel & OpenFL
+	 * @param Path Example: `your/video/here.mp4`
+	 * @param Loop Loop the video.
+	 * @param PauseMusic Pause music until the video ends.
+	 */
+	public function playVideo(Path:String, Loop:Bool = false, PauseMusic:Bool = false):Void
+		bitmap.playVideo(Path, Loop, PauseMusic);
+}
